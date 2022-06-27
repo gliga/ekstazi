e396b5dd5ff99b9f1ccc70d8d1ad3db71bdfb0ed
Yuki Liu <yuki.liu@utexas.edu>
2022-06-27 13:31:56 -0500

    implement skipping non-affected tests

--- org.ekstazi.core/src/main/java/org/ekstazi/junit/CoverageExtension.java
+++ org.ekstazi.core/src/main/java/org/ekstazi/junit/CoverageExtension.java
@@ -17,11 +17,14 @@
 package org.ekstazi.junit;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.Optional;
 
 import org.ekstazi.Config;
 import org.ekstazi.Ekstazi;
 import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.ConditionEvaluationResult;
+import org.junit.jupiter.api.extension.ExecutionCondition;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.InvocationInterceptor;
 import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
@@ -29,37 +32,37 @@ import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
 /**
  * Support for collecting coverage for test class granularity level.
  */
-public class CoverageExtension implements InvocationInterceptor, AfterAllCallback{
-    private Class mClz;
-
-    // @Override
-    // public void beforeAll(ExtensionContext context) throws Exception {  
-    //     if (isIgnoreAllTests() || !Ekstazi.inst().isClassAffected(mClz.getName())) {
-            
-    //     } 
-    // }
+public class CoverageExtension implements InvocationInterceptor, AfterAllCallback, ExecutionCondition {
 
     @Override
+    /** 
+     * tell Ekstazi to start collecting coverage for test class granularity level.
+     * @param invocation
+     * @param invocationContext
+     * @param extensionContext
+     */
     public <T> T interceptTestClassConstructor(Invocation<T> invocation,
             ReflectiveInvocationContext<Constructor<T>> invocationContext,
             ExtensionContext extensionContext)
             throws Throwable {
         T o = invocation.proceed();
-        mClz = o.getClass();
-        // System.out.println("interceptTestClassConstructor, class: " +
-        // mClz.getName());
-        Ekstazi.inst().beginClassCoverage(mClz.getName());
+        Ekstazi.inst().beginClassCoverage(o.getClass().getName());
         return o;
     }
 
     @Override
+    /**
+     * tell Ekstazi the execution result
+     * @param context
+     * @return
+     */
     public void afterAll(ExtensionContext context) throws Exception {
-        System.out.println("after all methods, class: " + mClz);
         Optional<Throwable> exception = context.getExecutionException();
+        String className = context.getTestClass().get().getName();
         if (exception.isPresent()) {
-            Ekstazi.inst().endClassCoverage(mClz.getName(), true);
+            Ekstazi.inst().endClassCoverage(className, true);
         } else {
-            Ekstazi.inst().endClassCoverage(mClz.getName(), false);
+            Ekstazi.inst().endClassCoverage(className, false);
         }
     }
 
@@ -70,6 +73,21 @@ public class CoverageExtension implements InvocationInterceptor, AfterAllCallbac
      */
     private boolean isIgnoreAllTests() {
         return Config.X_IGNORE_ALL_TESTS_V;
+    }
+
+    @Override
+    /**
+     * Skip the test if all tests are ignored or test is not affected.
+     * @param context
+     * @return
+     */
+    public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
+        if (isIgnoreAllTests() || (context.getTestClass().isPresent()
+                && !Ekstazi.inst().isClassAffected(context.getTestClass().get().getName()))) {
+            return ConditionEvaluationResult.disabled("Ekstazi deselected");
+        } else {
+            return ConditionEvaluationResult.enabled("Ekstazi selected");
+        }
     }
 
 }
